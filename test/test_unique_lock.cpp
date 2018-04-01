82a6d2c867ca7d07d412cd3fbeb4b49cecf0f967
huzhan <huzhan8787@gmail.com>
2018-04-01 11:04:00 +0800

    Add unique_lock test example

---
+++ test/test_unique_lock.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <thread>
+
+std::mutex mtx;
+
+void print(const int& num) {
+	std::unique_lock<std::mutex> lock(mtx);
+	std::cout << num << std::endl;
+}
+
+int main(int argc, char* argv[]) {
+	std::vector<std::thread> t_pool;
+	for (int i = 0; i < 10; ++i) {
+		t_pool.push_back(std::thread(print, i));
+	}
+	for (int i = 0; i < 10; ++i) {
+		if (t_pool[i].joinable()) {
+			t_pool[i].join();
+		}
+	}
+	return 0;
+}
